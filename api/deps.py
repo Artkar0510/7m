a4b10f4435024497f225f789868d1baa3c6a214e
Artkar0510 <Artkar00093@yandex.ru>
2026-04-27 16:33:16 +0300

    edit 4

--- api/deps.py
+++ api/deps.py
@@ -1,10 +1,15 @@
+import hmac
+
 from fastapi import Header, HTTPException, status
 
 from core.settings import settings
 
 
 def verify_internal_service(x_service_token: str = Header(...)) -> str:
-    if x_service_token != settings.internal_auth.service_token:
+    if not hmac.compare_digest(
+        x_service_token.encode(),
+        settings.internal_auth.service_token.encode()
+    ):
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
             detail="Invalid service credentials",
